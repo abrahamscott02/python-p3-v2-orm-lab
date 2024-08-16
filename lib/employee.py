145a2b7d2bf87ea6d6a63362c9bc3615e82ee3e5
Abraham Scott <abrahamscott02@gmail.com>
2024-08-15 18:31:04 -0600

    Done

--- lib/employee.py
+++ lib/employee.py
@@ -186,5 +186,17 @@ class Employee:
         return cls.instance_from_db(row) if row else None
 
     def reviews(self):
-        """Return list of reviews associated with current employee"""
-        pass
+        """Return a list of Review instances associated with the current employee."""
+    
+        from review import Review  # Embedded import to avoid circular import issue
+    
+        sql = """
+            SELECT *
+            FROM reviews
+            WHERE employee_id = ?
+         """
+    
+        rows = CURSOR.execute(sql, (self.id,)).fetchall()
+    
+        # Convert rows into Review instances
+        return [Review.instance_from_db(row) for row in rows]
